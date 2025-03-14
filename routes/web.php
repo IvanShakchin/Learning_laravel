0182e011b13b79b019e2cae67bd6386cbcbbb36c
ivan <ivanavatar@yandex.ru>
2025-03-14 07:02:37 +0300

    пройден 2 день изучения laravel маршрутизаторы

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
-
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +13,115 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// фасад Route содержит набор статичиских функций с функциями замыкания
+
+Route::get('/', function () { // все get запросы обрабатываются тут
+    return view('welcome');  // глобальная функция view запускает шаблон welcome
+});
+
+Route::get('/about', function () {  // get, post, put, patch, delete,
+    $x = 5 + 7;
+    echo $x."<br> Выводит что то <br>";
+    return $x + 5; 
+});
+
+Route::post('/about', function () { 
+    return 1; 
+});
+
+Route::match (['get','post'], 'contacts', function () { 
+    return 'contacts'; 
+}); // на этой странице мы get открваем стр. post обрабатыем данные
+
+Route::any ('feedback', function () { // обработает запросы любого вида
+    return 'feedback'; 
+}) ;
+
+//получение параметров url 
+Route::get('/post/{parametr1}/{parametr2}', function ($id_1,$id_2) { 
+    echo $id_1." - Выводит первый параметр<br>";
+    echo $id_2." - Выводит второй параметр<br>";
+    return ; 
+});
+
+// Выводит Request параметр
+Route::get('/user/{id}', function ($id, Request $request) { 
+    echo $request->path()." - Выводит Request параметр<br>";
+    return $id; 
+// регулярное выражение в where ограничит вывод тольоко цифры  
+//})-> where ('id', '[0-9]+'); 
+// если параметров несколько то делаем массив
+//})-> where (['id', '[0-9]+','comment', '[0-9]+']); 
+// варант через готовую функцию только цифры
+// })-> whereNumber ('id'); 
+// варант через готовую функцию только строка
+// })-> whereAlpha('id'); 
+// варант через готовую функцию число и строка
+})-> whereAlphaNumeric('id') ->name('user'); 
+// Задание имен для маршрутов прописывем фукция name()
+
+
+
+// Если нет параметра /user то {id?} и задается параметр по умолчанию $id = 1
+Route::get('/user/{id?}', function ($id = 1) { 
+    return "user: ". $id; 
+});
+
+// Создание груп маршрутов например группа manager
+Route::prefix ('manager')->group (function (){
+    Route::get('/', function () { 
+        return 'manager.index';  
+    });
+    Route::get('/users', function () { 
+        return 'manager.users';  
+    });
+});
+
+// Редирект с помощью маршрутизатора
+Route::redirect ('/myuser/', '/user/');
+Route::redirect ('/myuser/{id}', '/user/{id}');
+//Вариант постоянного редиректа permanentredirect
+// Route::permanentredirect ('/myuser/', '/user/');
+
+// products 
+
+
+// Route::match (['get','post'],'/products', function () { 
+
+//     $url = ((!empty($_SERVER['HTTPS'])) ? 'https' : 'http') . '://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+//     $url = explode('?', $url);
+//     $url = $url[0]; 
+//     return $url ; 
+
+// });
+
+
+Route::get('/product/{id}/{comment}', function ($id_1,$id_2) { 
+
+    $url = ((!empty($_SERVER['HTTPS'])) ? 'https' : 'http') . '://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+    $url = explode('?', $url);
+    $url = $url[0]; 
+    return $url ; 
+
+});
+
+Route::prefix ('admin')->group (function (){
+
+    Route::match (['get','post'], '/', function () { 
+        return 'admin.index';  
+    });
+
+
+    Route::match (['get','post'], '/auth', function () { 
+        return 'admin.auth';  
+    });
+
+    Route::match (['get','post'], '/products', function () { 
+        return 'admin.products';  
+    });
+
+    Route::match (['get','post'], '/clients', function () { 
+        return 'admin.clients';  
+    });
+
 });
